4e6836a1c4b8044ac8d1dc5bb0de3b80fa3be55f
G <52905881+giga-a@users.noreply.github.com>
2021-03-23 23:09:14 -0700

    Update auto-checking.sh

--- auto-checking.sh
+++ auto-checking.sh
@@ -5,13 +5,13 @@ date > clean-up.logs
 commit="false"
 
 if [[ "$1" == "social-analyzer" ]]; then
-    echo "[X] social-analyzer"
-    echo "[X] installing autopep8"
-    apt-get install -y python3-autopep8
-    echo "[X] installing autopep8"
+    echo "[X] social-analyzer" > clean-up.logs
+    echo "[X] installing autopep8 & jq" > clean-up.logs
+    apt-get install -y python3-autopep8 jq
+    echo "[X] running autopep8" > clean-up.logs
     autopep8 . --recursive --in-place --max-line-length=10000 --verbose --aggressive --ignore=E402
-    echo "[X] sorting websites"
+    echo "[X] sorting websites" > clean-up.logs
     jq '.websites_entries|=sort_by(.url)' ./data/sites.json > ./data/sites.json_new
     cp -f ./data/sites.json_new ./data/sites.json
-    commit="true"
+    echo "[X] done" > clean-up.logs
 fi
