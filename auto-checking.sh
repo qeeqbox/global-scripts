656ac7756e48ce954f39bc55cd6d4680bc206e4c
G <52905881+giga-a@users.noreply.github.com>
2021-03-24 22:35:20 -0700

    Update auto-checking.sh

--- auto-checking.sh
+++ auto-checking.sh
@@ -13,4 +13,11 @@ if [[ "$1" == "social-analyzer" ]]; then
     jq '.websites_entries|=sort_by(.url)' ./data/sites.json > ./data/sites.json_new
     cp -f ./data/sites.json_new ./data/sites.json
     echo "[X] done" > clean-up.logs
+elif [[ "$1" == "honeypots" ]]; then
+    echo "[X] honeypots" > clean-up.logs
+    echo "[X] installing autopep8 & jq" > clean-up.logs
+    sudo apt-get install -y python3-autopep8 jq
+    echo "[X] running autopep8" > clean-up.logs
+    autopep8 . --recursive --in-place --max-line-length=10000 --verbose --aggressive --ignore=E402
+    echo "[X] done" > clean-up.logs
 fi
