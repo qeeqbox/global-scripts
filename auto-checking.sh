297d94950bbf34504909813d9c3f2e129bd42e62
G <52905881+giga-a@users.noreply.github.com>
2021-04-02 23:28:14 -0700

    Update auto-checking.sh

--- auto-checking.sh
+++ auto-checking.sh
@@ -18,23 +18,21 @@ elif [[ "$1" == "honeypots" ]]; then
     echo "[X] installing python3, python3-pip, autopep8 & jq" >> clean-up.logs
     sudo apt-get install -y python3 python3-pip python3-autopep8 jq
     echo "[X] installing the pip package" >> clean-up.logs
-    cwd=$(pwd)
     mkdir -p /tmp/test
     cp -r . /tmp/test/
-    ls /tmp/test
     pip3 install /tmp/test
     echo "[X] checking the pip package" >> clean-up.logs
     temp_value=$(python3 -m honeypots list)
     echo "[X] delete " >> clean-up.logs
     find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
     if echo "$temp_value" | grep -q 'dns'; then
-        echo "[X] good package"
+        echo "[X] good package" >> clean-up.logs
         echo "[X] running autopep8" >> clean-up.logs
         autopep8 . --recursive --in-place --max-line-length=10000 --verbose --aggressive --ignore=E402
         echo "[X] done" >> clean-up.logs
 
     else
-        echo "[X] bad package"
+        echo "[X] bad package" >> clean-up.logs
         exit 1
     fi
 fi
